3f273a6ac07ed61ea079e2d8bf6c38b01fb027a6
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2021-02-17 14:40:48 +0200

    No diagonal

---
+++ QueenAttack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        if (white.Column == black.Column || white.Row == black.Row)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        if (row >= 0 && row < 8)
+        {
+            if (column >= 0 && column < 8)
+            {
+                return new Queen(row, column);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+    }
+}
